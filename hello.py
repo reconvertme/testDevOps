842ce56c3ca27eedfb9b52b2c3c6e46a7cd5ddc3
leopold <leopold.gamba@freesbee.fr>
2023-01-27 06:13:47 +0100

    Autres

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
 print("Hello le Monde entier")
+
+nom = "DUPONT"
+age = 12
+print("J'ai {nom} et j'ai {age} ans.")
+
+import math
+c = math.sqrt(16)
+print("Racine de 16 :", c)
+
+# utiliser le module
+import mymodule
+resultat = mymodule.somme(2,8)
+print(resultat)
